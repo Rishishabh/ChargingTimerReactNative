d46cb959cb7a61fb017372b5264f012c873e29a4
Rishishabh <rishabhnahata1@gmail.com>
2025-05-25 04:04:59 +0530

    Add files via upload

---
+++ android/app/src/main/java/com/chargingtimerreactnative/MainActivity.java
@@ -0,0 +1,10 @@
+package com.chargingtimerreactnative;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+    @Override
+    protected String getMainComponentName() {
+        return "ChargingTimerReactNative";
+    }
+}
