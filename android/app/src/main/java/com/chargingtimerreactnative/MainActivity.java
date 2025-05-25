e6416d7e270cb1dbf36c244c094ccb8a7d0f6847
Rishishabh <rishabhnahata1@gmail.com>
2025-05-25 16:38:36 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/chargingtimerreactnative/MainActivity.java
+++ android/app/src/main/java/com/chargingtimerreactnative/MainActivity.java
@@ -1,10 +1,19 @@
 package com.chargingtimer;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "ChargingTimerReactNative";
+    }
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Set the theme to ensure AppCompat is used
+        setTheme(R.style.AppTheme); // Ensure the correct theme is applied here
     }
 }
