382e3e900051b3c34ff55598ff7c1be453ec51ba
Rishishabh <rishabhnahata1@gmail.com>
2025-05-25 17:57:41 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/chargingtimerreactnative/MainActivity.java
+++ android/app/src/main/java/com/chargingtimerreactnative/MainActivity.java
@@ -1,8 +1,7 @@
 package com.chargingtimerreactnative;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -13,73 +12,47 @@ import com.facebook.react.uimanager.UIManagerModule;
 import com.facebook.react.bridge.JavaScriptModule;
 import java.util.Arrays;
 import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ReactInstanceManager mReactInstanceManager;
-    private ReactNativeHost mReactNativeHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Initialize React Native Instance Manager
-        mReactNativeHost = new ReactNativeHost(getApplication()) {
-            @Override
-            public boolean getUseDeveloperSupport() {
-                return BuildConfig.DEBUG;  // Set to true in Debug mode
-            }
+        // Initialize SoLoader for React Native
+        SoLoader.init(this, false);
+        
+        // Set the content view
+        setContentView(R.layout.activity_main); // Make sure you have activity_main.xml in res/layout
 
-            @Override
-            public List<ReactPackage> getPackages() {
-                return Arrays.<ReactPackage>asList(
-                        new MainReactPackage()  // Add necessary React packages here
-                );
-            }
-
-            @Override
-            public String getJSMainModuleName() {
-                return "index";  // Your JS entry point
-            }
-        };
-
-        // Initialize ReactInstanceManager with the ReactNativeHost
-        mReactInstanceManager = mReactNativeHost.getReactInstanceManager();
-
-        // SoLoader initialization
-        SoLoader.init(this, /* native exopackage */ false);
-
-        // Set content view here
-        setContentView(R.layout.activity_main);  // Ensure you have an activity_main.xml in res/layout
-
-        // Start React Activity (React Native setup)
-        mReactInstanceManager.createReactContextInBackground();
+        // ReactActivityDelegate to handle lifecycle events
+        ReactActivityDelegate delegate = new ReactActivityDelegate(this, getMainComponentName());
+        delegate.onCreate(savedInstanceState);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        // Manually handle React Native lifecycle
-        if (mReactInstanceManager != null) {
-            mReactInstanceManager.onResume(this);
-        }
+        ReactActivityDelegate delegate = new ReactActivityDelegate(this, getMainComponentName());
+        delegate.onResume();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        // Manually handle React Native lifecycle
-        if (mReactInstanceManager != null) {
-            mReactInstanceManager.onPause(this);
-        }
+        ReactActivityDelegate delegate = new ReactActivityDelegate(this, getMainComponentName());
+        delegate.onPause();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        // Manually handle React Native lifecycle
-        if (mReactInstanceManager != null) {
-            mReactInstanceManager.onDestroy(this);
-        }
+        ReactActivityDelegate delegate = new ReactActivityDelegate(this, getMainComponentName());
+        delegate.onDestroy();
+    }
+
+    protected String getMainComponentName() {
+        return "ChargingTimerReactNative"; // This is your JavaScript entry point (index.js)
     }
 }
